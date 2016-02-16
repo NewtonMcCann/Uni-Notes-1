25bf010bd8e72744a463e1e1143c830c4b85a736
Andrew <andrew.pheasey@gmail.com>
2016-02-16 14:50:19 +0000

    Push Uni-Notes from Newt to Group.

---
+++ app/src/main/java/com/javahorizon/uni_notes/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.javahorizon.uni_notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends PageActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
---
+++ app/src/main/java/com/javahorizon/uni_notes/PageActivity.java
@@ -0,0 +1,10 @@
+package com.javahorizon.uni_notes;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Luke McCann on 13/02/2016.
+ */
+
+public abstract class PageActivity extends AppCompatActivity {
+}
